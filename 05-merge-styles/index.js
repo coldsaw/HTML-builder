19bc3da27ef977b7476df529e650e714b5121dde
Viktoria Sinyaeva <sinyaeva.viktoria@gmail.com>
2024-01-20 07:19:36 +0300

    feat: complete fifth task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const source = path.join(__dirname, 'styles');
+const destination = path.join(__dirname, 'project-dist');
+
+const output = fs.createWriteStream(
+  path.join(destination, 'bundle.css'),
+  'utf-8',
+);
+
+(async () => {
+  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      if (
+        !file.isDirectory() &&
+        file.name.split('.')[file.name.split('.').length - 1] === 'css'
+      ) {
+        const stream = fs.createReadStream(path.join(file.path, file.name), {
+          encoding: 'utf-8',
+        });
+        stream.on('data', (chunk) => output.write(chunk));
+      }
+    }
+  });
+})();
